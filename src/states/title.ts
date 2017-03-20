c65138724837921bb7d756bb89449d5e74463ab0
Jeff McKenzie <mail@mcknz.com>
2017-03-20 17:39:03 -0400

    started adding weapon to title class

--- src/states/title.ts
+++ src/states/title.ts
@@ -2,11 +2,17 @@ import * as Assets from '../assets';
 
 export default class Title extends Phaser.State {
     private projectileSpritesheet: Phaser.Sprite = null;
+    private weapon: Phaser.Weapon = null;
 
     public preload(): void {
+
+        let gameX: number = this.game.world.centerX;
+        let gameY: number = this.game.world.centerY;
+
+        this.addProjectiles(gameX, gameY);
         this.projectileSpritesheet = this.game.add.sprite(
-            this.game.world.centerX,
-            this.game.world.centerY,
+            gameX,
+            gameY,
             Assets.Spritesheets.SpritesheetsProjectileSprites.getName()
         );
         this.projectileSpritesheet.animations.add('projectile', [56]);
@@ -15,5 +21,10 @@ export default class Title extends Phaser.State {
 
     public create(): void {
         // this.game.camera.flash(0x000000, 1000);
+    }
+
+    private addProjectiles(x: number, y: number): void {
+        this.weapon = this.add.weapon(10, 'bullet'); 
+        this.weapon.fireFrom.setTo(x, y, 1,1);
     }
 }
